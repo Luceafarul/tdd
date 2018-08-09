86ec8814afc16b0140bb850cc82cf1ba234cbc18
Yaroslav <yaroslav.leo@gmail.com>
2018-08-09 21:48:00 +0300

    Remove side effect from times() method.

--- src/test/java/exchange/DollarTest.java
+++ src/test/java/exchange/DollarTest.java
@@ -2,12 +2,24 @@ package exchange;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DollarTest {
     @Test void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar ten = five.times(2);
+        assertEquals(10, ten.amount);
+    }
+
+    @Test void testMultiplicationTwoTimes() {
+        Dollar five = new Dollar(5);
+
+        Dollar ten = five.times(2);
+        Dollar fifteen = five.times(3);
+        assertAll(
+                () -> assertEquals(10, ten.amount),
+                () -> assertEquals(15, fifteen.amount)
+        );
     }
 }
