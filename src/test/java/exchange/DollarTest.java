6e2a488b22e98be3f3d935262dc7894c970aebff
Yaroslav <yaroslav.leo@gmail.com>
2018-08-11 22:47:48 +0300

    Make amount variable private, update tests, remove unnecessary test.

--- src/test/java/exchange/DollarTest.java
+++ src/test/java/exchange/DollarTest.java
@@ -5,27 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DollarTest {
-    @Test void testMultiplication() {
-        Dollar five = new Dollar(5);
-        Dollar ten = five.times(2);
-        assertEquals(10, ten.amount);
-    }
-
     @Test void testMultiplicationTwoTimes() {
         Dollar five = new Dollar(5);
-
-        Dollar ten = five.times(2);
-        Dollar fifteen = five.times(3);
         assertAll(
-                () -> assertEquals(10, ten.amount),
-                () -> assertEquals(15, fifteen.amount)
+            () -> assertEquals(new Dollar(10), five.times(2)),
+            () -> assertEquals(new Dollar(15), five.times(3))
         );
     }
 
     @Test void testEquality() {
         assertAll(
-                () -> assertEquals(new Dollar(5), new Dollar(5)),
-                () -> assertNotEquals(new Dollar(5), new Dollar(7))
+            () -> assertEquals(new Dollar(5), new Dollar(5)),
+            () -> assertNotEquals(new Dollar(5), new Dollar(7))
         );
     }
 
